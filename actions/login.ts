8d1fe071a0e2432621023cd4e7dab2fe6a6d8118
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-05 18:54:11 +0800

    Add configuration for user login credentials

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import { AuthError } from "next-auth";
 
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -13,7 +16,27 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     };
   }
 
-  return {
-    success: "Email sent successfully",
-  };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Invalid email or password",
+          };
+        default:
+          return {
+            error: "Something went wrong",
+          };
+      }
+    }
+    throw error;
+  }
 };
