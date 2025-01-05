c0d8664bbb36f0cad2aec6dc7136ec37fea45d8a
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-05 16:20:45 +0800

    Prevent duplicate email registration for existing users

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -13,7 +16,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     };
   }
 
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "Email already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
   return {
-    success: "Email sent successfully",
+    success: "Account created successfully",
   };
 };
