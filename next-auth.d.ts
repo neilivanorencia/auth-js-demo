d73c760bbcb58bf275b85e7918ab1416461d4d57
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-06 11:48:10 +0800

    Extend session with user role and ID

---
+++ next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & { role: "ADMIN" | "USER" };
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role?: "ADMIN" | "USER";
+  }
+}
