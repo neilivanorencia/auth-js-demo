67628e88c8389d96dc68fecc21c424e6ae2f7227
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-06 22:27:57 +0800

    Add GitHub authentication client

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 import { getUserByEmail } from "@/data/user";
@@ -7,6 +9,11 @@ import { LoginSchema } from "@/schemas";
 
 export default {
   providers: [
+    Google,
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
