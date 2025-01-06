6df5d918aa3ed2867b56f47e737932ab23c03f53
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-06 23:07:15 +0800

    Auto-verify GitHub and Google OAuth accounts

--- auth.ts
+++ auth.ts
@@ -6,6 +6,18 @@ import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,23 +2,31 @@
 
 import { FcGoogle } from "react-icons/fc";
 import { SiGithub } from "react-icons/si";
+import { signIn } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex w-full items-center gap-x-2">
       <Button
         size="lg"
-        className="shadow-none w-full border-2 border-mint-500 bg-transparent transition duration-500 ease-in-out  hover:bg-celadon-500/80 hover:shadow-lg hover:shadow-celadon-500/40 active:translate-y-0.5 active:shadow-none"
-        onClick={() => {}}
+        className="w-full border-2 border-mint-500 bg-transparent shadow-none transition duration-500 ease-in-out hover:bg-celadon-500/80 hover:shadow-lg hover:shadow-celadon-500/40 active:translate-y-0.5 active:shadow-none"
+        onClick={() => onClick("google")}
       >
         <FcGoogle />
       </Button>
       <Button
         size="lg"
-        className="shadow-none w-full border-2 border-mint-500 bg-transparent transition duration-500 ease-in-out  hover:bg-celadon-500/80 hover:shadow-lg hover:shadow-celadon-500/40 active:translate-y-0.5 active:shadow-none"
-        onClick={() => {}}
+        className="w-full border-2 border-mint-500 bg-transparent shadow-none transition duration-500 ease-in-out hover:bg-celadon-500/80 hover:shadow-lg hover:shadow-celadon-500/40 active:translate-y-0.5 active:shadow-none"
+        onClick={() => onClick("github")}
       >
         <SiGithub color="black" />
       </Button>
