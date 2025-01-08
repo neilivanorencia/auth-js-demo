063a28f77b68503b051d7c2d0fab1a90f75e2b36
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-08 20:27:47 +0800

    Setup password reset token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,32 @@
 import { v4 as uuid } from "uuid";
 
 import { db } from "@/lib/db";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuid();
+  const expires = new Date(new Date().getTime() + 10 * 60 * 1000); // 10 minutes from now
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuid();
