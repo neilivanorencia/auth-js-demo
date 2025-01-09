96fe638e535fd2ee78133a5614ff77704a82435d
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-09 16:29:30 +0800

    Add schemas for password reset

--- schemas/index.ts
+++ schemas/index.ts
@@ -52,6 +52,27 @@ export const RegisterSchema = z.object({
     }),
 });
 
+export const ResetPassword = z.object({
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters long",
+    })
+    .refine((password) => /[A-Z]/.test(password), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((password) => /[a-z]/.test(password), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .refine((password) => /[0-9]/.test(password), {
+      message: "Password must contain at least one number",
+    })
+    .refine((password) => /[!@#$%^&*]/.test(password), {
+      message:
+        "Password must contain at least one special character (!@#$%^&*)",
+    }),
+});
+
 export const ResetSchema = z.object({
   email: z.string().email({
     message: "Email is required",
