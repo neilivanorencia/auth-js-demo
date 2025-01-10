00422ec56e9b1dbfe50714a484351bfefa8dd43a
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-10 21:31:34 +0800

    Add function to generate and manage two-factor authentication tokens

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,9 @@
+import crypto from "crypto";
 import { v4 as uuid } from "uuid";
 
 import { db } from "@/lib/db";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 
 export const generatePasswordResetToken = async (email: string) => {
@@ -26,6 +28,30 @@ export const generatePasswordResetToken = async (email: string) => {
   });
 
   return passwordResetToken;
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 10 * 60 * 1000); // 10 minutes from now
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
 };
 
 export const generateVerificationToken = async (email: string) => {
