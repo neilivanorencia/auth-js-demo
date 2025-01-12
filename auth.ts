7b3c1f29f4256316c2ef8d07e252ceeb57f7f775
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-12 17:56:27 +0800

    Update two-factor enabled status

--- auth.ts
+++ auth.ts
@@ -63,6 +63,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.role = token.role;
       }
 
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -73,6 +77,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,6 +1,9 @@
 import NextAuth, { type DefaultSession } from "next-auth";
 
-export type ExtendedUser = DefaultSession["user"] & { role: "ADMIN" | "USER" };
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+  isTwoFactorEnabled: boolean;
+};
 
 declare module "next-auth" {
   interface Session {
