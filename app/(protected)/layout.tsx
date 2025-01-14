49d22a772ec7e095b9ff515f7b3e835bd1e85e09
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-15 06:42:48 +0800

    Fix mobile responsiveness of the protected layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,9 +6,12 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-y-4 px-4 [background-image:radial-gradient(#ffffff33_1px,transparent_1px),linear-gradient(to_bottom_right,rgb(16_185_129),rgb(20_184_166),rgb(8_145_178))] [background-position:0_0,0_0] [background-size:16px_16px,100%_100%]">
-      <NavigationBar />
-      {children}
+    <div className="flex min-h-screen w-full flex-col">
+      <div className="fixed inset-0 [background-image:radial-gradient(#ffffff33_1px,transparent_1px),linear-gradient(to_bottom_right,rgb(16_185_129),rgb(20_184_166),rgb(8_145_178))] [background-position:0_0,0_0] [background-size:16px_16px,100%_100%]" />
+      <main className="relative flex flex-1 flex-col items-center justify-center gap-y-4 overflow-y-auto px-4 py-4">
+        <NavigationBar />
+        {children}
+      </main>
     </div>
   );
 };
