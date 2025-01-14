eb0ca3abe73de3907a106d89db5d732185307200
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-14 10:53:19 +0800

    Modify and update user session settings accordingly

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 import { getUserById } from "@/data/user";
+import { getAccountByUserId } from "@/data/account";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -67,6 +68,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -76,6 +83,11 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
