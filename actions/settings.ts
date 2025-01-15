611d65ef185b2776cbf1d03f5939c32cd18faa90
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-15 19:02:08 +0800

    Update settings page

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,21 +1,72 @@
 "use server";
 
 import * as z from "zod";
-import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
 
+import { db } from "@/lib/db";
 import { currentUser } from "@/lib/auth";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
 import { SettingsSchema } from "@/schemas";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
+
   if (!user) {
     return { error: "User not found" };
   }
 
   const dbUser = await getUserById(user.id!);
+
   if (!dbUser) {
     return { error: "User not found" };
+  }
+
+  if (user.isOAuth) {
+    const { name, role, isTwoFactorEnabled } = values;
+
+    await db.user.update({
+      where: { id: dbUser.id },
+      data: {
+        name,
+        role,
+        isTwoFactorEnabled,
+      },
+    });
+
+    return { success: "Settings updated" };
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "Email already exists" };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token,
+    );
+
+    return { success: "Verification email sent" };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordsMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password,
+    );
+
+    if (!passwordsMatch) {
+      return { error: "Incorrect password" };
+    }
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined as unknown as string;
   }
 
   await db.user.update({
@@ -26,5 +77,6 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
       ...values,
     },
   });
+
   return { success: "Settings updated" };
 };
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -15,7 +15,6 @@ import {
   FormControl,
   FormDescription,
   FormField,
-  FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
@@ -31,26 +30,27 @@ import {
 } from "@/components/ui/select";
 import { settings } from "@/actions/settings";
 import { SettingsSchema } from "@/schemas";
-
-import { IoSettingsOutline } from "react-icons/io5";
+import { Switch } from "@/components/ui/switch";
 import { UserRole } from "@prisma/client";
 
+import { IoSettingsOutline } from "react-icons/io5";
+
 const SettingsPage = () => {
+  const user = useCurrentUser();
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
-  const [isPending, startTransition] = useTransition();
   const { update } = useSession();
-
-  const user = useCurrentUser();
+  const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof SettingsSchema>>({
     resolver: zodResolver(SettingsSchema),
     defaultValues: {
-      name: user?.name || undefined,
-      email: user?.email || undefined,
       password: undefined,
       newPassword: undefined,
+      name: user?.name || undefined,
+      email: user?.email || undefined,
       role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
     },
   });
 
@@ -58,22 +58,53 @@ const SettingsPage = () => {
     startTransition(() => {
       settings(values)
         .then((data) => {
-          if (data?.error) {
+          if (data.error) {
             setError(data.error);
           }
-          if (data?.success) {
+          if (data.success) {
+            update();
             setSuccess(data.success);
           }
         })
-        .catch(() => setError("Something went wrong"));
+        .catch(() => setError("Something went wrong!"));
     });
   };
 
+  const commonFields = [
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      placeholder: "Enter name",
+    },
+  ];
+
+  const nonOAuthFields = [
+    {
+      name: "email",
+      label: "Email",
+      type: "email",
+      placeholder: "Enter email",
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+      placeholder: "Enter password",
+    },
+    {
+      name: "newPassword",
+      label: "New Password",
+      type: "password",
+      placeholder: "Enter new password",
+    },
+  ];
+
   return (
-    <Card className="mx-auto w-full max-w-[900px] bg-gradient-to-br from-slate-50 via-slate-50/90 to-slate-100/80 transition-all duration-300 ease-in-out hover:shadow-xl">
+    <Card className="w-full max-w-[900px] bg-gradient-to-br from-sky-50 via-teal-50/90 to-emerald-50/80 transition duration-300 ease-in-out hover:shadow-xl">
       <CardHeader className="space-y-4 p-4 sm:p-6">
         <div className="flex items-center justify-center space-x-3">
-          <IoSettingsOutline className="h-6 w-6 text-slate-700 sm:h-8 sm:w-8" />
+          <IoSettingsOutline className="h-6 w-6 sm:h-8 sm:w-8" />
           <h2 className="text-xl font-semibold text-slate-700 sm:text-2xl">
             Settings
           </h2>
@@ -82,92 +113,86 @@ const SettingsPage = () => {
           Manage and modify set of information
         </p>
       </CardHeader>
-      <CardContent>
+      <CardContent className="pt-6">
         <Form {...form}>
-          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="space-y-4">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-0.5">
+            {commonFields.map((field) => (
               <FormField
+                key={field.name}
                 control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="Enter new name here"
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                  </FormItem>
+                name={field.name as any}
+                render={({ field: formField }) => (
+                  <div className="grid grid-cols-1 gap-y-1 rounded-lg transition-colors hover:bg-slate-50/50 md:grid-cols-2 md:gap-y-0">
+                    <div className="flex items-center gap-x-2 p-1.5 text-slate-600 md:p-3">
+                      <FormLabel className="text-xs font-medium md:text-sm">
+                        {field.label}
+                      </FormLabel>
+                    </div>
+                    <div className="rounded-md bg-white/80 p-1.5 md:rounded-none md:bg-transparent md:p-3">
+                      <FormControl>
+                        <Input
+                          {...formField}
+                          placeholder={field.placeholder}
+                          type={field.type}
+                          disabled={isPending}
+                          className="border-slate-200 text-xs text-slate-800 md:text-sm"
+                        />
+                      </FormControl>
+                    </div>
+                  </div>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="Enter new email here"
-                        type="email"
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="Enter new password here"
-                        type="password"
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="newPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>New Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="Enter new password here"
-                        type="password"
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="role"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Role</FormLabel>
+            ))}
+
+            {!user?.isOAuth &&
+              nonOAuthFields.map((field) => (
+                <FormField
+                  key={field.name}
+                  control={form.control}
+                  name={field.name as any}
+                  render={({ field: formField }) => (
+                    <div className="grid grid-cols-1 gap-y-1 rounded-lg transition-colors hover:bg-slate-50/50 md:grid-cols-2 md:gap-y-0">
+                      <div className="flex items-center gap-x-2 p-1.5 text-slate-600 md:p-3">
+                        <FormLabel className="text-xs font-medium md:text-sm">
+                          {field.label}
+                        </FormLabel>
+                      </div>
+                      <div className="rounded-md bg-white/80 p-1.5 md:rounded-none md:bg-transparent md:p-3">
+                        <FormControl>
+                          <Input
+                            {...formField}
+                            placeholder={field.placeholder}
+                            type={field.type}
+                            disabled={isPending}
+                            className="border-slate-200 text-xs text-slate-800 md:text-sm"
+                          />
+                        </FormControl>
+                      </div>
+                    </div>
+                  )}
+                />
+              ))}
+
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field: formField }) => (
+                <div className="grid grid-cols-1 gap-y-1 rounded-lg transition-colors hover:bg-slate-50/50 md:grid-cols-2 md:gap-y-0">
+                  <div className="flex items-center gap-x-2 p-1.5 text-slate-600 md:p-3">
+                    <FormLabel className="text-xs font-medium md:text-sm">
+                      Role
+                    </FormLabel>
+                  </div>
+                  <div className="rounded-md bg-white/80 p-1.5 md:rounded-none md:bg-transparent md:p-3">
                     <Select
                       disabled={isPending}
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                      value={field.value}
+                      onValueChange={formField.onChange}
+                      defaultValue={formField.value}
+                      value={formField.value}
                     >
                       <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select a role" />
+                        <SelectTrigger className="border-slate-200 text-xs text-slate-800 md:text-sm">
+                          <SelectValue placeholder="Select role" />
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
@@ -180,15 +205,48 @@ const SettingsPage = () => {
                       </SelectContent>
                     </Select>
                     <FormMessage />
-                  </FormItem>
+                  </div>
+                </div>
+              )}
+            />
+
+            <div className="border-b px-4 py-3">
+              <FormField
+                control={form.control}
+                name="isTwoFactorEnabled"
+                render={({ field }) => (
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <FormLabel className="text-sm font-medium text-slate-600">
+                        Two Factor Authentication
+                      </FormLabel>
+                      <FormDescription className="text-xs text-slate-500">
+                        Enable 2FA for enhanced security
+                      </FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        disabled={isPending}
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </div>
                 )}
               />
             </div>
-            <FormError message={error} />
-            <FormSuccess message={success} />
-            <Button type="submit" disabled={isPending}>
-              Save
-            </Button>
+
+            <div className="mt-6 space-y-4">
+              <FormError message={error} />
+              <FormSuccess message={success} />
+              <Button
+                type="submit"
+                disabled={isPending}
+                className="w-full bg-teal-500 transition duration-300 ease-in-out hover:bg-teal-400"
+              >
+                Save changes
+              </Button>
+            </div>
           </form>
         </Form>
       </CardContent>
